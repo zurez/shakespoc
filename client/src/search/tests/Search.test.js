dec23440bea52c4ff06a4aaf9269f25d22c8c2f5
Md Zurez Tuba <zurez@users.noreply.github.com>
2022-05-23 02:54:24 +0530

    Update Search.test.js

--- client/src/search/tests/Search.test.js
+++ client/src/search/tests/Search.test.js
@@ -1,12 +1,37 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import * as hooks from "../../hooks/useFetchPokemon.hook";
 import Search from "../Search";
 
-describe.skip("Tests for the search page", () => {
+describe("Tests for the search page", () => {
+  beforeEach(() => {});
   it("tests for the presence of button and input on the search page", async () => {
     render(<Search />);
     await waitFor(() => {
       expect(screen.getByText("Find your pokemon!")).toBeInTheDocument();
     });
     expect(screen.getByLabelText("eg: Pikachu")).toBeInTheDocument();
+  });
+
+  it("tests for calling setPokeName method from the hook with the input value", async () => {
+    render(<Search />);
+    const setPokemonName = jest.fn();
+    jest.spyOn(hooks, "useFetchPokemon").mockReturnValueOnce({
+      data: null,
+      setPokemonName,
+      error: null,
+      loading: false,
+    });
+
+    const input = screen.getByLabelText("eg: Pikachu");
+    fireEvent.change(input, { target: { value: "pikachu" } });
+    expect(input.value).toBe("pikachu");
+
+    const button = screen.getByText("Find your pokemon!");
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(setPokemonName).toHaveBeenCalledTimes(1);
+    });
   });
 });
