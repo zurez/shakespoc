0eb509fe3fe56ef924451936341608d43277ee9b
Md Zurez Tuba <zurez@users.noreply.github.com>
2022-05-23 03:28:07 +0530

    Update Search.test.js

--- client/src/search/tests/Search.test.js
+++ client/src/search/tests/Search.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import * as hooks from "../../hooks/useFetchPokemon.hook";
+import SearchInput from "../components/SearchInput";
 import Search from "../Search";
 
 describe("Tests for the search page", () => {
@@ -13,25 +14,19 @@ describe("Tests for the search page", () => {
   });
 
   it("tests for calling setPokeName method from the hook with the input value", async () => {
-    render(<Search />);
-    const setPokemonName = jest.fn();
-    jest.spyOn(hooks, "useFetchPokemon").mockReturnValueOnce({
-      data: null,
-      setPokemonName,
-      error: null,
-      loading: false,
-    });
+    const onClick = jest.fn();
+    const loading = false;
+
+    render(<SearchInput onClick={onClick} loading={loading} />);
 
     const input = screen.getByLabelText("eg: Pikachu");
     fireEvent.change(input, { target: { value: "pikachu" } });
     expect(input.value).toBe("pikachu");
 
-    const button = screen.getByText("Find your pokemon!");
+    const button = screen.getByText(/Find your pokemon!/i);
+
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
-
-    await waitFor(() => {
-      expect(setPokemonName).toHaveBeenCalledTimes(1);
-    });
+    expect(onClick).toHaveBeenCalledTimes(1);
   });
 });
